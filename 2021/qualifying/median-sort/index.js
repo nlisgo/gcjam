e3f1bf49711e58831a2b10c1cbc6566c2dc43c59
nlisgo <nathan@aeopus.com>
2021-04-01 17:59:30 +0100

    Slight optimisation

--- 2021/qualifying/median-sort/index.js
+++ 2021/qualifying/median-sort/index.js
@@ -5,7 +5,6 @@ const rl = readline.createInterface(process.stdin);
 let [T, N, Q] = [0, 0, 0];
 const answersGiven = [];
 let organise = [];
-let centreIndex = 1;
 let positionIndex = 0;
 let queries = [];
 let response;
@@ -31,6 +30,7 @@ rl.on('line', (line) => {
     if (answersGiven.length < T) {
         if (queries.length === 0) {
             postQuery([1, 2, 3]);
+            positionIndex = 0;
             valueToPlace = 4;
             valuesToPlace = [...Array(N - 4).fill().map((_, i) => i + 5)];
             valuePlaced = false;
@@ -40,54 +40,37 @@ rl.on('line', (line) => {
                 organise = [...queries[0].filter(item => item !== response)];
                 organise.push(organise[1]);
                 organise[1] = response;
-                centreIndex = 1;
-                positionIndex = 0;
-                postQuery([organise[centreIndex], organise[positionIndex], valueToPlace]);
+                postQuery([organise[positionIndex], organise[positionIndex + 1], valueToPlace]);
             } else {
-                if (response === queries[queries.length - 1][2]) {
-                    if (positionIndex < centreIndex) {
-                        // valueToPlace in positionIndex + 1
-                        organise = organise.slice(0, positionIndex + 1).concat([valueToPlace], organise.slice(positionIndex + 1));
-                        valuePlaced = true;
-                    } else {
-                        // valueToPlace in positionIndex - 1
-                        organise = organise.slice(0, positionIndex).concat([valueToPlace], organise.slice(positionIndex));
-                        valuePlaced = true;
-                    }
-                } else if (response === queries[queries.length - 1][1]) {
-                    if (positionIndex === 0) {
-                        // valueToPlace at 0
-                        organise = [valueToPlace].concat(organise);
-                        valuePlaced = true;
-                    } else if (positionIndex === organise.length - 1) {
-                        // valueToPlace at end
-                        organise = organise.concat([valueToPlace]);
-                        valuePlaced = true;
-                    } else {
-                        if (positionIndex < centreIndex) {
-                            positionIndex--;
-                        } else {
-                            positionIndex++;
-                        }
-                    }
-                } else if (response === queries[queries.length - 1][0]) {
-                    if (positionIndex < centreIndex) {
-                        positionIndex = centreIndex + 1;
-                    } else {
-                        positionIndex = centreIndex - 1;
-                    }
+                if (response === queries[queries.length - 1][0]) {
+                    // valueToPlace just before positionIndex
+                    organise = (positionIndex === 0 ? [] : organise.slice(0, positionIndex)).concat([valueToPlace], organise.slice(positionIndex));
+                    valuePlaced = true;
+                } else if (response === queries[queries.length - 1][2]) {
+                    // valueToPlace just after positionIndex
+                    organise = organise.slice(0, positionIndex + 1).concat([valueToPlace], organise.slice(positionIndex + 1));
+                    valuePlaced = true;
+                } else if (organise.length === positionIndex + 2) {
+                    // valueToPlace at end
+                    organise = organise.concat([valueToPlace]);
+                    valuePlaced = true;
                 }
     
                 if (organise.length < N) {
                     if (valuePlaced) {
-                        centreIndex = Math.floor(organise.length / 2);
-                        positionIndex = centreIndex - 1;
+                        positionIndex = 0;
                         valueToPlace = valuesToPlace[0];
                         valuesToPlace = [...valuesToPlace.slice(1)];
                         valuePlaced = false;
+                    } else {
+                        positionIndex += 2;
+                    }
+
+                    if (positionIndex >= organise.length - 1) {
+                        positionIndex--;
                     }
     
-                    postQuery([organise[centreIndex], organise[positionIndex], valueToPlace]);
+                    postQuery([organise[positionIndex], organise[positionIndex + 1], valueToPlace]);
                 } else {
                     postAnswer(organise);
                     queries = [];
