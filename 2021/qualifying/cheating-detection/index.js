d5a9eb3b42f61c4be6a8785c862a988666e3c581
nlisgo <nathan@aeopus.com>
2021-04-08 19:35:09 +0100

    Solve test set 1

--- 2021/qualifying/cheating-detection/index.js
+++ 2021/qualifying/cheating-detection/index.js
@@ -2,13 +2,36 @@ Number.prototype.toCase = function () {
     return this.valueOf()+1;
 };
 
+const scorePerson = answers => score(answers);
+const scoreQuestion = answers => score(answers);
+
+const score = answers => scoreCount(answers) / answers.length;
+
+const scoreCount = answers => answers.reduce((score, x) => (x == 1 ? score + 1 : score), 0);
+
+const transpose = array => array[0].map((r, i) => array.map(c => c[i]));
+
+const indexOfMaxValue = a => a.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+
 const solve = input => {
-	return input;
+	const scoresPerson = input.map(row => scorePerson(row.split('')));
+	// const scoresQuestion = [...transpose(input.map(row => row.split('')))].map(scoreQuestion);
+
+	return indexOfMaxValue(scoresPerson) + 1;
 };
 
 const solveInputs = inputs => {
 	const cases = [];
-	inputs.slice(1).forEach((data, i) => {
+	const rows = [];
+	let row = [];
+	for (let i = 2; i < inputs.length; i++) {
+		row.push(inputs[i]);
+		if (row.length === 100) {
+			rows.push(row);
+			row = [];
+		}
+	}
+	rows.forEach((data, i) => {
 		cases.push(`Case #${i.toCase()}: ${solve(data)}`);
 	});
 	return cases;
@@ -39,4 +62,7 @@ if (!Boolean(process.stdin.isTTY)) {
 module.exports = {
 	solve,
 	solveInputs,
+	scorePerson,
+	scoreQuestion,
+	transpose,
 };
