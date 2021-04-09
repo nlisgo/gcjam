38bada647f6c02a60fb88dc106fe598ca9faf2f2
nlisgo <nathan@aeopus.com>
2021-04-09 16:43:12 +0100

    Rank participants of difference between easiest questions and hard questions

--- 2021/qualifying/cheating-detection/index.js
+++ 2021/qualifying/cheating-detection/index.js
@@ -11,43 +11,50 @@ const scoreCount = answers => answers.reduce((score, x) => (x == 1 ? score + 1 :
 
 const transpose = array => array[0].map((r, i) => array.map(c => c[i]));
 
-const sigmoid = t => (1 / (1 + Math.pow(Math.E, -t)));
-
 const solve = input => {
 	const scoresPerson = input.map((row, i) => {
 		return {
 			p: i,
 			score: scorePerson(row),
-			rating: 0,
-			sig: 1,
+			percentageOfHardest: 0,
+			percentageOfEasiest: 0,
+			diffHardestEasiest: 0,
 		};
-	})
-	.sort((a, b) => (a.score < b.score) ? 1 : ((b.score < a.score) ? -1 : 0));
+	});
 
 	let scoresQuestion = [...transpose(input)].map((col, i) => {
 		return {
 			q: i,
 			score: scoreQuestion(col),
-			rating: 0,
 		};
-	}).sort((a, b) => (a.score > b.score) ? 1 : ((b.score > a.score) ? -1 : 0));
+	})
+	.sort((a, b) => (a.score > b.score) ? 1 : ((b.score > a.score) ? -1 : 0));
 
-	const rating = 3;
-	for (let i = 0; i < scoresQuestion.length; i++) {
-		scoresQuestion[i].rating = rating - ((i * rating * 2) / (scoresQuestion.length - 1));
+	const hardestQuestions = [];
+	for (let i = 0; i < Math.ceil(scoresQuestion.length * .05); i ++) {
+		hardestQuestions.push(scoresQuestion[i]);
 	}
 
+	const easiestQuestions = [];
+	for (let i = 0; i < Math.ceil(scoresQuestion.length * .05); i ++) {
+		easiestQuestions.push(scoresQuestion[scoresQuestion.length - 1 - i]);
+	}
+	
 	for (let i = 0; i < scoresPerson.length; i++) {
-		scoresPerson[i].rating = rating - ((i * rating * 2) / (scoresPerson.length - 1));
-		for (let j = 0; j < Math.min(scoresQuestion.length, 100); j++) {
-			let sig = sigmoid(scoresPerson[i].rating - scoresQuestion[j].rating);
-			scoresPerson[i].sig *= (input[scoresPerson[i].p] === '1') ? sig : 1 - sig;
+		let h = 0;
+		let e = 0
+		for (let j = 0; j < hardestQuestions.length; j++) {
+			h += Number(input[i][hardestQuestions[j].q]);
 		}
+		for (let j = 0; j < easiestQuestions.length; j++) {
+			e += Number(input[i][easiestQuestions[j].q]);
+		}
+		scoresPerson[i].percentageOfHardest = h / hardestQuestions.length;
+		scoresPerson[i].percentageOfEasiest = e / easiestQuestions.length;
+		scoresPerson[i].diffHardestEasiest = scoresPerson[i].percentageOfEasiest - scoresPerson[i].percentageOfHardest;
 	}
 
-	const sigsPerson = [...scoresPerson.sort((a, b) => (a.sig > b.sig) ? 1 : ((b.sig > a.sig) ? -1 : 0))];
-
-	return sigsPerson[0].p + 1;
+	return scoresPerson.sort((a, b) => (a.diffHardestEasiest > b.diffHardestEasiest) ? 1 : ((b.diffHardestEasiest > a.diffHardestEasiest) ? -1 : 0))[0].p + 1;
 };
 
 const solveInputs = inputs => {
