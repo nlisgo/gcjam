7f1a27b04938da0a2d101b0baf77b2d91bd5fddc
nlisgo <nathan@aeopus.com>
2022-03-12 02:54:59 +0000

    various refactors

--- 2019/qualifying/foregone-solution/index.js
+++ 2019/qualifying/foregone-solution/index.js
@@ -1,41 +1,20 @@
-Number.prototype.toCase = function () {
-    return this.valueOf()+1;
-};
-
 const solve = input => [checkA(input), checkB(input)].join(' ');
+const solveInputs = inputs => inputs.slice(1).map(solve).map((c, i) => `Case #${i+1}: ${c}`);
 
 const checkA = input => input.replaceAll(/4/g, '3');
 const checkB = input => input.replaceAll(/4/g, 'x').replaceAll(/[^x]/g, '0').replace(/^[0]+/, '').replaceAll(/[x]/g, '1');
 
-const solveInputs = inputs => {
-	const cases = [];
-	let i = 0;
-	inputs.slice(1).forEach((data) => {
-        cases.push(`Case #${i.toCase()}: ${solve(data)}`);
-		i++;
-	});
-	return cases;
-};
-
-const handleStdin = () => {
-	const readline = require('readline');
-	const inputs = [];
-
-	const rl = readline.createInterface({
-	    input: process.stdin,
-	});
-
-	rl.on('line', line => {
-		inputs.push(line);
-	}).on('close', () => {
-		solveInputs(inputs).forEach(out => {
-			console.log(out);
-		});
-	});
-};
-
 if (!Boolean(process.stdin.isTTY)) {
-	handleStdin();
+    const lines = [];
+	require('readline').createInterface({
+	    input: process.stdin,
+	}).on('line', line => {
+		lines.push(line);
+	}).on('close', () => {
+        solveInputs(lines).forEach(line => {
+            console.log(line);
+        });
+	});
 }
 
 module.exports = {
