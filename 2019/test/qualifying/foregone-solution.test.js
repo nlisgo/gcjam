8c9b2a4f2bd39a9ba349cebd327166b97fca4494
nlisgo <nathan@aeopus.com>
2022-03-12 02:56:43 +0000

    Remove white space

--- 2019/test/qualifying/foregone-solution.test.js
+++ 2019/test/qualifying/foregone-solution.test.js
@@ -4,11 +4,11 @@ describe('solve', () => {
     test('4', () => {
         expect(foregoneSolution.solve('4')).toBe('3 1');
     });
-    
+
     test('940', () => {
         expect(foregoneSolution.solve('940')).toBe('930 10');
     });
-    
+
     test('4444', () => {
         expect(foregoneSolution.solve('4444')).toBe('3333 1111');
     });
