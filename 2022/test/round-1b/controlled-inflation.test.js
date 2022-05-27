709f5212d3c67caab5a2502e6c0ade91f55b304c
nlisgo <nathan@aeopus.com>
2022-05-27 01:31:35 +0100

    Introduce tests and sample input

--- 2022/test/round-1b/controlled-inflation.test.js
+++ 2022/test/round-1b/controlled-inflation.test.js
@@ -3,52 +3,76 @@ const controlledInflation = require('../../round-1b/controlled-inflation/index')
 describe('splitInput', () => {
     test('sample.in', () => {
         expect(
-            controlledInflation.splitInput(`3
-one
-two
-three`.split(/\r?\n/))
+            controlledInflation.splitInput(`2
+3 3
+30 10 40
+20 50 60
+60 60 50
+5 2
+1 1000000000
+500000000 1000000000
+1 1000000000
+500000000 1
+1 1000000000`.split(/\r?\n/))
         )
         .toStrictEqual(
             [
-                'one',
-                'two',
-                'three',
+                [
+                    [30, 10, 40],
+                    [20, 50, 60],
+                    [60, 60, 50],
+                ],
+                [
+                    [1, 1000000000],
+                    [500000000, 1000000000],
+                    [1, 1000000000],
+                    [500000000, 1],
+                    [1, 1000000000],
+                ],
             ]
         );
     });
 });
 
 describe('solve', () => {
-    test('one', () => {
-        expect(controlledInflation.solve('one')).toBe('one');
+    test('[30, 10, 40], [20, 50, 60], [60, 60, 50]', () => {
+        expect(controlledInflation.solve([
+            [30, 10, 40],
+            [20, 50, 60],
+            [60, 60, 50],
+        ])).toBe(110);
     });
 
-    test('two', () => {
-        expect(controlledInflation.solve('two')).toBe('two');
-    });
-
-    test('three', () => {
-        expect(controlledInflation.solve('three')).toBe('three');
-    });
-
-    test('four', () => {
-        expect(controlledInflation.solve('four')).toBe('four');
+    test('[1, 1000000000], [500000000, 1000000000], [1, 1000000000], [500000000, 1], [1, 1000000000]', () => {
+        expect(controlledInflation.solve([
+            [1, 1000000000],
+            [500000000, 1000000000],
+            [1, 1000000000],
+            [500000000, 1],
+            [1, 1000000000],
+        ])).toBe(4999999996);
     });
 });
 
 describe('solveInputs', () => {
     test('sample.in', () => {
         expect(
-            controlledInflation.solveInputs(`3
-one
-two
-three`.split(/\r?\n/))
+            controlledInflation.solveInputs(`2
+3 3
+30 10 40
+20 50 60
+60 60 50
+5 2
+1 1000000000
+500000000 1000000000
+1 1000000000
+500000000 1
+1 1000000000`.split(/\r?\n/))
         )
         .toStrictEqual(
             [
-                'Case #1: one',
-                'Case #2: two',
-                'Case #3: three',
+                'Case #1: 110',
+                'Case #2: 4999999996',
             ]
         );
     });
